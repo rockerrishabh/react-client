04aaca06bc1d5d20a85814fee9274702c4d5d9f9
Rishabh Kumar <rishabh.6542@gmail.com>
2024-12-28 16:37:55 +0530

    fourth commit

--- src/routes/dashboard.lazy.tsx
+++ src/routes/dashboard.lazy.tsx
@@ -1,19 +1,16 @@
-import { useAuth } from "@/hooks/useAuth";
-import { createLazyFileRoute, useNavigate } from "@tanstack/react-router";
-import { useEffect } from "react";
-
+import { createLazyFileRoute } from "@tanstack/react-router";
 export const Route = createLazyFileRoute("/dashboard")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const { auth } = useAuth();
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (!auth) {
-      navigate({ to: "/sign-in" });
-    }
-  }, [auth, navigate]);
+  // const { auth } = useAuth();
+  // const navigate = useNavigate();
+  // useEffect(() => {
+  //   if (!auth) {
+  //     navigate({ to: "/sign-in" });
+  //   }
+  // }, [auth, navigate]);
 
   return <div>Hello "/dashboard"!</div>;
 }
